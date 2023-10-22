39f45a4f1e80da2a8d713750e3d10d330864e336
utas-raymondng <manfai.ng@utas.edu.au>
2023-10-22 16:38:49 +1100

    use diff approach to cqlfilterfactory

---
+++ server/src/main/java/au/org/aodn/ogcapi/server/core/parser/ElasticFilter.java
@@ -0,0 +1,9 @@
+package au.org.aodn.ogcapi.server.core.parser;
+
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+
+public abstract class ElasticFilter {
+    protected Query query;
+
+    public Query getQuery() { return query; }
+}
