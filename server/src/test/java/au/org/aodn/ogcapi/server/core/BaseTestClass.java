1b70fd3eb7589c08b699cd3f37838aed08018673
utas-raymondng <manfai.ng@utas.edu.au>
2023-12-11 12:18:56 +1100

    First auto test case work

--- server/src/test/java/au/org/aodn/ogcapi/server/core/BaseTestClass.java
+++ server/src/test/java/au/org/aodn/ogcapi/server/core/BaseTestClass.java
@@ -3,17 +3,23 @@ package au.org.aodn.ogcapi.server.core;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.util.ResourceUtils;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.*;
 
@@ -39,6 +45,8 @@ public class BaseTestClass {
     @Value("${elasticsearch.index.name}")
     protected String INDEX_NAME;
 
+    protected Logger logger = LoggerFactory.getLogger(BaseTestClass.class);
+
     protected String getBasePath() {
         return "http://localhost:" + port + "/api/v1/ogc";
     }
@@ -48,6 +56,7 @@ public class BaseTestClass {
     }
 
     protected void clearElasticIndex() throws IOException {
+        logger.debug("Clear elastic index");
         try {
             client.deleteByQuery(f -> f
                     .index(INDEX_NAME)
@@ -76,12 +85,28 @@ public class BaseTestClass {
             File j = ResourceUtils.getFile("classpath:databag/" + filename);
 
             try(Reader reader = new FileReader(j)) {
-                client.index(i -> i
+                IndexResponse indexResponse = client.index(i -> i
                         .index(INDEX_NAME)
-                        .withJson(reader)
-                );
+                        .withJson(reader));
+
+                logger.info("Sample file {}, indexed with response : {}", filename, indexResponse);
             }
         }
+        // Must all, otherwise index is not rebuild immediately
+        client.indices().refresh();
+
+        // Check the number of doc store inside the ES instance is correct
+        SearchRequest.Builder b = new SearchRequest.Builder()
+                .index(INDEX_NAME)
+                        .query(QueryBuilders.matchAll().build()._toQuery());
+
+        SearchRequest request = b.build();
+        logger.debug("Elastic search payload for verification {}", request.toString());
+
+        SearchResponse<ObjectNode> response = client.search(request, ObjectNode.class);
+        logger.debug(response.toString());
+
+        assertEquals(filenames.length, response.hits().hits().size(), "Number of docs stored is correct");
     }
 
     protected Response getClusterHealth() throws IOException {
--- server/src/test/java/au/org/aodn/ogcapi/server/core/common/RestApiIT.java
+++ server/src/test/java/au/org/aodn/ogcapi/server/core/common/RestApiIT.java
@@ -3,8 +3,7 @@ package au.org.aodn.ogcapi.server.core.common;
 import au.org.aodn.ogcapi.features.model.Collections;
 import au.org.aodn.ogcapi.server.core.BaseTestClass;
 import org.junit.AfterClass;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -15,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RestApiIT extends BaseTestClass {
 
     @AfterClass
@@ -23,22 +23,28 @@ public class RestApiIT extends BaseTestClass {
     }
 
     @AfterEach
-    public void afterEach() throws IOException {
+    public void afterTest() throws IOException {
         super.clearElasticIndex();
     }
 
     @Test
+    @Order(1)
     public void verifyClusterIsHealthy() throws IOException {
         super.assertClusterHealthResponse();
     }
-
+    /**
+     * The search is a fuzzy search based on title and description. So you expect 1 hit only
+     * @throws IOException
+     */
     @Test
-    public void verifyApiCollectionsQuery() throws IOException {
-        super.insertJsonToElasticIndex("516811d7-cd1e-207a-e0440003ba8c79dd.json");
+    public void verifyApiCollectionsQuery1() throws IOException {
+        super.insertJsonToElasticIndex(
+                "516811d7-cd1e-207a-e0440003ba8c79dd.json",
+                "7709f541-fc0c-4318-b5b9-9053aa474e0e.json"
+        );
 
         // Call rest api directly and get query result
         ResponseEntity<Collections> collections = testRestTemplate.getForEntity(getBasePath() + "/collections?q=reproduction", Collections.class);
-
-        assertEquals(1, collections.getBody().getCollections().size(), "Contain 1 item");
+        assertEquals(1, collections.getBody().getCollections().size(), "Only 1 hit");
     }
 }
