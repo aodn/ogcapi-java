ed342bf8c236748fffecbf7edfe6fd2a808a8047
utas-raymondng <manfai.ng@utas.edu.au>
2023-12-12 11:19:54 +1100

    Add feature rest api test

---
+++ server/src/test/java/au/org/aodn/ogcapi/server/core/features/RestApiTest.java
@@ -0,0 +1,60 @@
+package au.org.aodn.ogcapi.server.core.features;
+
+import au.org.aodn.ogcapi.features.model.Collection;
+import au.org.aodn.ogcapi.server.core.BaseTestClass;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)         // We need to use @BeforeAll @AfterAll with not static method
+public class RestApiTest extends BaseTestClass {
+
+    @AfterAll
+    public void afterClass() {
+        super.shutdownElasticSearch();
+    }
+
+    @BeforeAll
+    public void beforeClass() throws IOException {
+        super.createElasticIndex();
+    }
+
+    @BeforeEach
+    public void afterTest() throws IOException {
+        super.clearElasticIndex();
+    }
+
+    @Test
+    @Order(1)
+    public void verifyClusterIsHealthy() throws IOException {
+        super.assertClusterHealthResponse();
+    }
+
+    @Test
+    public void verifyGetSingleCollection() throws IOException {
+        super.insertJsonToElasticIndex(
+                "516811d7-cd1e-207a-e0440003ba8c79dd.json",
+                "7709f541-fc0c-4318-b5b9-9053aa474e0e.json"
+        );
+
+        // Call rest api directly and get query result
+        ResponseEntity<Collection> collection = testRestTemplate.getForEntity(
+                getBasePath() + "/collections/516811d7-cd1e-207a-e0440003ba8c79dd",
+                Collection.class);
+
+        assertNotNull(collection.getBody(), "Body not null");
+        assertEquals(
+                "516811d7-cd1e-207a-e0440003ba8c79dd",
+                collection.getBody().getId(),
+                "Correct UUID - 516811d7-cd1e-207a-e0440003ba8c79dd");
+    }
+}
