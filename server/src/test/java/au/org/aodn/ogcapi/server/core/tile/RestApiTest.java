e33aef8c80a6d2b2588ff8816311d5c591248c72
utas-raymondng <manfai.ng@utas.edu.au>
2023-12-12 17:01:16 +1100

    Add 1 more tile test

--- server/src/test/java/au/org/aodn/ogcapi/server/core/tile/RestApiTest.java
+++ server/src/test/java/au/org/aodn/ogcapi/server/core/tile/RestApiTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -56,4 +57,41 @@ public class RestApiTest extends BaseTestClass {
         assertEquals("Ocean acidification historical reconstruction", tiles.getBody().getTilesets().get(0).getTitle(), "Title matched 1");
         assertEquals("Impacts of stress on coral reproduction.", tiles.getBody().getTilesets().get(1).getTitle(), "Title matched 2");
     }
+    /**
+     * Verify api call /collections/{collectionId}/tiles
+     */
+    @Test
+    public void verifyCollectionTiles() throws IOException {
+        super.insertJsonToElasticIndex(
+                "516811d7-cd1e-207a-e0440003ba8c79dd.json",
+                "7709f541-fc0c-4318-b5b9-9053aa474e0e.json"
+        );
+
+        ResponseEntity<InlineResponse2002> tiles = testRestTemplate.getForEntity(
+                getBasePath() + "/collections/516811d7-cd1e-207a-e0440003ba8c79dd/tiles",
+                InlineResponse2002.class
+        );
+
+        assertEquals(1, tiles.getBody().getTilesets().size(), "Count is correct");
+        assertEquals("Impacts of stress on coral reproduction.", tiles.getBody().getTilesets().get(0).getTitle(), "Title matched 1");
+    }
+    /**
+     * Verify api call /tiles/{tileMatrixSetId}/{tileMatrix}/{tileRow}/{tileCol}, this call will return the bounding
+     * box using mvt search, however this mvt search require licence and not enable by default. Without UI it is
+     * very hard to enable it.
+     */
+    @Test
+    public void verifyTilesMatrixSetXYZ() throws IOException {
+        super.insertJsonToElasticIndex(
+                "b299cdcd-3dee-48aa-abdd-e0fcdbb9cadc.json"
+        );
+
+        ResponseEntity<byte[]> tiles = testRestTemplate.getForEntity(
+                getBasePath() + "/tiles/WebMercatorQuad/2/0/1?collections=b299cdcd-3dee-48aa-abdd-e0fcdbb9cadc",
+                byte[].class
+        );
+
+        // No meaningful verify can be done here, we keep it here so that we know that test case considered.
+    }
+
 }
