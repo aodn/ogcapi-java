3a7e0cf74252d63ea08f1b2aee53d60d9648fc45
Viet Nguyen <vh.nguyen@utas.edu.au>
2024-03-01 17:13:04 +1100

    fix typo

--- server/src/main/java/au/org/aodn/ogcapi/server/common/CommonRestExtApi.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/common/CommonRestExtApi.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController("CommonRestExtApi")
-@RequestMapping(value = "/api/v1/ogcapi/ext")
+@RequestMapping(value = "/api/v1/ogc/ext")
 public class CommonRestExtApi {
 
     @Autowired
