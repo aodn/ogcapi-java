2faf491fdcd7f5e6491d8dcc66bdedf6dc94699b
Viet Nguyen <vietnguyenswin@outlook.com>
2024-07-27 22:32:14 +1000

    suggesting phrases instead of titles

--- server/src/main/java/au/org/aodn/ogcapi/server/core/model/RecordSuggestDTO.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/core/model/RecordSuggestDTO.java
@@ -1,10 +1,14 @@
 package au.org.aodn.ogcapi.server.core.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
 
 @Data
 @Builder
@@ -12,5 +16,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RecordSuggestDTO {
-    private String title;
+    private List<String> abstractPhrases;
+
+    @JsonProperty("record_suggest")
+    private void unpackRecordSuggestNode(Map<String, List<String>> recordSuggest) {
+        this.abstractPhrases = recordSuggest.get("abstract_phrases");
+    }
 }
