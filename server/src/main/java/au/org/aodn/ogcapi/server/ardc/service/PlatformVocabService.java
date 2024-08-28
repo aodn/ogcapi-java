564fed719e63b66a830595cde18149ef88ec9c9d
Viet Nguyen <vietnguyenswin@outlook.com>
2024-08-28 10:32:58 +1000

    Finalise getPlatformVocabs() method

--- server/src/main/java/au/org/aodn/ogcapi/server/ardc/service/PlatformVocabService.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/ardc/service/PlatformVocabService.java
@@ -3,17 +3,13 @@ package au.org.aodn.ogcapi.server.ardc.service;
 import au.org.aodn.ogcapi.server.ardc.model.PlatformVocabModel;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -24,16 +20,13 @@ public class PlatformVocabService {
     @Autowired
     protected RestTemplate ardcVocabRestTemplate;
 
-    protected static String platformVocabApiPath = "/aodn-platform-category-vocabulary/version-1-2/concept.json";
-
-    protected static String leafPath = "/aodn-platform-vocabulary/version-6-1/concept.json";
-
-    protected static String details = "/aodn-platform-vocabulary/version-6-1/resource.json?uri=%s";
+    protected static String platformCategoryVocabApi = "/aodn-platform-category-vocabulary/version-1-2/concept.json";
+    protected static String platformVocabApi = "/aodn-platform-vocabulary/version-6-1/concept.json";
+    protected static String resourceDetailsApi = "/aodn-platform-vocabulary/version-6-1/resource.json?uri=%s";
 
     protected Function<JsonNode, String> label = (node) -> node.get("prefLabel").get("_value").asText();
     protected Function<JsonNode, String> about = (node) -> node.has("_about") ? node.get("_about").asText() : null;
     protected Function<JsonNode, String> definition = (node) -> node.has("definition") ? node.get("definition").asText() : null;
-
     protected BiFunction<JsonNode, String, Boolean> isNodeValid = (node, item) -> node != null && !node.isEmpty() && node.has(item) && !node.get(item).isEmpty();
 
     /**
@@ -46,13 +39,11 @@ public class PlatformVocabService {
      * @param vocabApiBase
      * @return
      */
-    protected Map<String, List<PlatformVocabModel>> getLeafNodeOfParameterVocab(String vocabApiBase) {
+    protected Map<String, List<PlatformVocabModel>> getPlatformVocabLeafNodes(String vocabApiBase) {
         Map<String, List<PlatformVocabModel>> results = new HashMap<>();
-        String url = String.format(vocabApiBase + leafPath);
+        String url = String.format(vocabApiBase + platformVocabApi);
 
-        while (url != null) {
-            log.debug("Query api -> {}", url);
-
+        while (url != null && !url.isEmpty()) {
             ObjectNode r = ardcVocabRestTemplate.getForObject(url, ObjectNode.class);
             if (r != null && !r.isEmpty()) {
                 JsonNode node = r.get("result");
@@ -60,7 +51,7 @@ public class PlatformVocabService {
                 if (isNodeValid.apply(node, "items")) {
                     for (JsonNode j : node.get("items")) {
                         // Now we need to construct link to detail resources
-                        String dl = String.format(vocabApiBase + details, about.apply(j));
+                        String dl = String.format(vocabApiBase + resourceDetailsApi, about.apply(j));
                         try {
                             log.debug("Query api -> {}", dl);
                             ObjectNode d = ardcVocabRestTemplate.getForObject(dl, ObjectNode.class);
@@ -68,20 +59,31 @@ public class PlatformVocabService {
                             if(isNodeValid.apply(d, "result") && isNodeValid.apply(d.get("result"), "primaryTopic")) {
                                 JsonNode target = d.get("result").get("primaryTopic");
 
-                                PlatformVocabModel model = PlatformVocabModel
+                                PlatformVocabModel vocab = PlatformVocabModel
                                         .builder()
                                         .label(label.apply(target))
                                         .definition(definition.apply(target))
                                         .about(about.apply(target))
                                         .build();
 
-                                if(target.has("broadMatch") && !target.get("broadMatch").isEmpty()) {
-                                    for(JsonNode bm : target.get("broadMatch")) {
-                                        if (!results.containsKey(bm.asText())) {
-                                            results.put(bm.asText(), new ArrayList<>());
+                                List<PlatformVocabModel> vocabNarrower = new ArrayList<>();
+                                if(target.has("narrower") && !target.get("narrower").isEmpty()) {
+                                    for(JsonNode narrower : target.get("narrower")) {
+                                        if (narrower.has("_about")) {
+                                            PlatformVocabModel narrowerNode = buildVocabByResourceUri(about.apply(narrower), vocabApiBase);
+                                            if (narrowerNode != null) {
+                                                vocabNarrower.add(narrowerNode);
+                                            }
                                         }
-                                        // We will have multiple cat under the same parent
-                                        results.get(bm.asText()).add(model);
+                                    }
+                                }
+                                if (!vocabNarrower.isEmpty()) {
+                                    vocab.setNarrower(vocabNarrower);
+                                }
+
+                                if (target.has("broadMatch") && !target.get("broadMatch").isEmpty()) {
+                                    for(JsonNode bm : target.get("broadMatch")) {
+                                        results.computeIfAbsent(bm.asText(), k -> new ArrayList<>()).add(vocab);
                                     }
                                 }
                             }
@@ -106,44 +108,34 @@ public class PlatformVocabService {
         return results;
     }
 
-    protected PlatformVocabModel buildPlatformVocabModel(JsonNode currentNode, JsonNode outerNode) {
-        if (currentNode instanceof ObjectNode objectNode) {
-            if (objectNode.has("prefLabel") && objectNode.has("_about")) {
-                return PlatformVocabModel.builder()
-                        .about(about.apply(currentNode))
-                        .label(label.apply(currentNode))
+    protected PlatformVocabModel buildVocabByResourceUri(String uri, String vocabApiBase) {
+        String detailsUrl = String.format(vocabApiBase + resourceDetailsApi, uri);
+        try {
+            log.debug("Query api -> {}", detailsUrl);
+            ObjectNode detailsObj = ardcVocabRestTemplate.getForObject(detailsUrl, ObjectNode.class);
+            if(isNodeValid.apply(detailsObj, "result") && isNodeValid.apply(detailsObj.get("result"), "primaryTopic")) {
+                JsonNode target = detailsObj.get("result").get("primaryTopic");
+                return PlatformVocabModel
+                        .builder()
+                        .label(label.apply(target))
+                        .definition(definition.apply(target))
+                        .about(uri)
                         .build();
             }
-        } else if (currentNode instanceof TextNode textNode) {
-            if (textNode.asText().contains("parameter_classes")) {
-                return PlatformVocabModel.builder()
-                        .about(textNode.asText())
-                        .label(this.findLabelByAbout(outerNode, textNode.asText()))
-                        .build();
-            }
-        }
-        return null;
-    }
-
-    protected String findLabelByAbout(JsonNode node, String c) {
-        for (JsonNode item : node.get("items")) {
-            if (about.apply(item).contains(c)) {
-                return label.apply(item);
-            }
+        } catch(Exception e) {
+            log.error("Item not found in resource {}", detailsUrl);
         }
         return null;
     }
 
     public List<PlatformVocabModel> getPlatformVocabs(String vocabApiBase) {
-        Map<String, List<PlatformVocabModel>> leaves = getLeafNodeOfParameterVocab(vocabApiBase);
+        Map<String, List<PlatformVocabModel>> leafNodes = getPlatformVocabLeafNodes(vocabApiBase);
         List<PlatformVocabModel> results = new ArrayList<>();
 
-        String url = String.format(vocabApiBase + platformVocabApiPath);
+        String url = String.format(vocabApiBase + platformCategoryVocabApi);
 
-        while (url != null) {
+        while (url != null && !url.isEmpty()) {
             try {
-                log.debug("Query api -> {}", url);
-
                 ObjectNode r = ardcVocabRestTemplate.getForObject(url, ObjectNode.class);
 
                 if (r != null && !r.isEmpty()) {
@@ -151,41 +143,21 @@ public class PlatformVocabService {
 
                     if (!node.isEmpty() && node.has("items") && !node.get("items").isEmpty()) {
                         for (JsonNode j : node.get("items")) {
-                            List<PlatformVocabModel> broader = new ArrayList<>();
+
                             List<PlatformVocabModel> narrower = new ArrayList<>();
-
-
-                            log.debug("Processing label {}", label.apply(j));
-
-                            if (j.has("broader")) {
-                                for (JsonNode b : j.get("broader")) {
-                                    broader.add(this.buildPlatformVocabModel(b, node));
-                                }
+                            if (leafNodes.containsKey(about.apply(j))) {
+                                narrower = leafNodes.get(about.apply(j));
                             }
 
-                            if (j.has("narrower")) {
-                                for (JsonNode b : j.get("narrower")) {
-                                    PlatformVocabModel c = this.buildPlatformVocabModel(b, node);
-                                    // The record comes from ardc have two levels only, so the second level for sure
-                                    // is empty, but the third level info comes form another link (aka the leaves)
-                                    // and therefore we can attach it to the second level to for the third.
-                                    if(leaves.containsKey(c.getAbout())) {
-                                        c.setNarrower(leaves.get(c.getAbout()));
-                                    }
-                                    narrower.add(c);
-                                }
-                            }
-
-                            PlatformVocabModel model = PlatformVocabModel
+                            PlatformVocabModel vocab = PlatformVocabModel
                                     .builder()
                                     .label(label.apply(j))
                                     .definition(definition.apply(j))
                                     .about(about.apply(j))
-                                    .broader(broader)
                                     .narrower(narrower)
                                     .build();
 
-                            results.add(model);
+                            results.add(vocab);
                         }
                     }
 
