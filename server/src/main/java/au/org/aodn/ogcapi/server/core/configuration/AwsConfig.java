2a24741ff5e9797c9a2bc4aa9c024a74e8d6fb3a
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-01-30 11:41:01 +1100

    fix for failure test about aws

--- server/src/main/java/au/org/aodn/ogcapi/server/core/configuration/AwsConfig.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/core/configuration/AwsConfig.java
@@ -3,6 +3,7 @@ package au.org.aodn.ogcapi.server.core.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.batch.BatchClient;
 
 @Configuration
@@ -12,6 +13,7 @@ public class AwsConfig {
     public BatchClient batchClient() {
         return BatchClient
                 .builder()
+                .region(Region.AP_SOUTHEAST_2)
                 .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
                 .build();
     }
