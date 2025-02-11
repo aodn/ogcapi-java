a7bb194a2fdf4815d8dd8fe68c7033767b4f5e81
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-02-11 11:24:47 +1100

    remove useless methods

--- server/src/main/java/au/org/aodn/ogcapi/server/processes/RestServices.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/processes/RestServices.java
@@ -6,12 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import software.amazon.awssdk.services.batch.BatchClient;
-import software.amazon.awssdk.services.batch.model.*;
+import software.amazon.awssdk.services.batch.model.SubmitJobRequest;
+import software.amazon.awssdk.services.batch.model.SubmitJobResponse;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -52,16 +50,6 @@ public class RestServices {
 
     private String submitJob(String jobName, String jobQueue, String jobDefinition, Map<String, String> parameters) {
 
-        List<KeyValuePair> environmentVariables = parameters.entrySet().stream()
-                .map(entry ->
-                        KeyValuePair
-                                .builder()
-                                .name(entry.getKey())
-                                .value(entry.getValue())
-                                .build()
-                )
-                .toList();
-
         SubmitJobRequest submitJobRequest = SubmitJobRequest.builder()
                 .jobName(jobName)
                 .jobQueue(jobQueue)
@@ -72,37 +60,4 @@ public class RestServices {
         SubmitJobResponse submitJobResponse = batchClient.submitJob(submitJobRequest);
         return submitJobResponse.jobId();
     }
-
-
-
-    // TODO: This feature doesn't work yet. Will be implemented in the future as this one is not urgent
-    public boolean isJobQueueValid(String jobQueueName) throws IOException {
-
-        var remoteJobQueueDetail = getRemoteJobQueueBy(jobQueueName);
-        var localJobQueueDetail = getLocalJobQueueDetailBy(jobQueueName);
-
-        return remoteJobQueueDetail.equals(localJobQueueDetail);
-    }
-
-    public JobQueueDetail getRemoteJobQueueBy(String name) {
-        var request = DescribeJobQueuesRequest
-                .builder()
-                .jobQueues(name)
-                .build();
-
-        var jobQueues = batchClient.describeJobQueues(request).jobQueues();
-
-        if (jobQueues != null && jobQueues.size() == 1) {
-            return jobQueues.get(0);
-        }
-        return null;
-    }
-
-    public JobQueueDetail getLocalJobQueueDetailBy(String jobQueueName) throws IOException {
-        var configJsonPath = "server/src/main/java/au/org/aodn/ogcapi/server/processes/config/" + jobQueueName + ".json";
-        var jsonFile = new File(configJsonPath);
-        var jsonStr = objectMapper.writeValueAsString(objectMapper.readValue(jsonFile, Object.class));
-        return objectMapper.readValue(jsonStr, JobQueueDetail.class);
-    }
-
 }
