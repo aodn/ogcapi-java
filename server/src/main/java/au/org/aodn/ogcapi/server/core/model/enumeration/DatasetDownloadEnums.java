dc75990f4f0d2d51817e1a78b8ab3060f12edc6a
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-09-25 14:22:30 +1000

    rollback unexpected change

--- server/src/main/java/au/org/aodn/ogcapi/server/core/model/enumeration/DatasetDownloadEnums.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/core/model/enumeration/DatasetDownloadEnums.java
@@ -38,7 +38,7 @@ public class DatasetDownloadEnums {
 
     @Getter
     public enum JobDefinition {
-        GENERATE_CSV_DATA_FILE("generate-csv-data-file-dev");
+        GENERATE_CSV_DATA_FILE("generate-csv-data-file");
 
         private final String value;
 
