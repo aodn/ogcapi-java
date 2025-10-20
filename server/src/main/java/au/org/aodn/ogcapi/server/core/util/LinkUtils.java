a83afb07bd171c4ddc9f4f0c81a6d9cfad2acc1f
Yuxuan HU <lydiahu_hn@hotmail.com>
2025-10-20 18:27:14 +1100

    update parse function to skip associated resource links

--- server/src/main/java/au/org/aodn/ogcapi/server/core/util/LinkUtils.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/core/util/LinkUtils.java
@@ -3,7 +3,7 @@ package au.org.aodn.ogcapi.server.core.util;
 import static au.org.aodn.ogcapi.server.core.util.ConstructUtils.constructByJsonString;
 
 public class LinkUtils {
-    private record TitleWithDescription(String title, String description) {}
+    record TitleWithDescription(String title, String description) {}
 
     public static String[] parseLinkTitleDescription(String combinedTitle) {
         // if combinedTitle is null or empty, return null for both title and description
@@ -14,9 +14,16 @@ public class LinkUtils {
         var titleWithDescription = constructByJsonString(combinedTitle, TitleWithDescription.class);
         if (titleWithDescription.isPresent()) {
             var titleDescription = titleWithDescription.get();
-            String title = (titleDescription.title() == null || titleDescription.title().trim().isEmpty()) ? null : titleDescription.title().trim();
-            String description = (titleDescription.description() == null || titleDescription.description().trim().isEmpty()) ? null : titleDescription.description().trim();
-            return new String[]{title, description};
+
+            // return parsed title and description if the combined text has these fields
+            if (titleDescription.description() != null) {
+                String title = (titleDescription.title() == null || titleDescription.title().trim().isEmpty()) ? null : titleDescription.title().trim();
+                String description = titleDescription.description().trim().isEmpty() ? null : titleDescription.description().trim();
+                return new String[]{title, description};
+            }
+            else {
+                return new String[]{combinedTitle.trim(), null};
+            }
         }
 
         // if not match, fallback to the original combinedTitle as title with null description
