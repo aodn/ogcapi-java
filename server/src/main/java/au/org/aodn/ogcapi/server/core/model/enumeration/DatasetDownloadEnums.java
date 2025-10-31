3fe8f440d06acd79e0c470b46d6d82b8b0bf9b97
amber <yujie.bian@utas.edu.au>
2025-10-31 15:41:01 +1100

    remove test

--- server/src/main/java/au/org/aodn/ogcapi/server/core/model/enumeration/DatasetDownloadEnums.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/core/model/enumeration/DatasetDownloadEnums.java
@@ -41,7 +41,7 @@ public class DatasetDownloadEnums {
 
     @Getter
     public enum JobDefinition {
-        GENERATE_CSV_DATA_FILE("generate-csv-data-file-dev");
+        GENERATE_CSV_DATA_FILE("generate-csv-data-file");
 
         private final String value;
 
