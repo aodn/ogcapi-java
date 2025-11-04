10a6e77e5a4bde2df76bd015e3a61abd09950807
rng <manfai.ng@utas.edu.au>
2025-11-04 13:33:45 +1100

    Add 1 test case

--- server/src/test/java/au/org/aodn/ogcapi/server/service/wfs/WfsServerTest.java
+++ server/src/test/java/au/org/aodn/ogcapi/server/service/wfs/WfsServerTest.java
@@ -1,5 +1,6 @@
 package au.org.aodn.ogcapi.server.service.wfs;
 
+import au.org.aodn.ogcapi.server.core.model.LinkModel;
 import au.org.aodn.ogcapi.server.core.model.StacCollectionModel;
 import au.org.aodn.ogcapi.server.core.model.ogc.wms.LayerInfo;
 import au.org.aodn.ogcapi.server.core.service.ElasticSearchBase;
@@ -7,7 +8,11 @@ import au.org.aodn.ogcapi.server.core.service.Search;
 import au.org.aodn.ogcapi.server.core.service.wfs.DownloadableFieldsService;
 import au.org.aodn.ogcapi.server.core.service.wfs.WfsServer;
 import au.org.aodn.ogcapi.server.core.util.RestTemplateUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
@@ -19,15 +24,32 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class WfsServerTest {
+
+    @Mock
+    Search mockSearch;
+
+    @Mock
+    DownloadableFieldsService downloadableFieldsService;
+
+    @Mock
+    RestTemplate restTemplate;
+
+    AutoCloseable closeableMock;
+
+    @BeforeEach
+    public void setUp() {
+        closeableMock = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterEach
+    void cleanUp() throws Exception {
+        closeableMock.close();
+    }
     /**
      * Test null case where the dataset have the collection id not found
      */
     @Test
     void noCollection_returnsEmptyLayers() {
-        Search mockSearch = mock(Search.class);
-        DownloadableFieldsService downloadableFieldsService = mock(DownloadableFieldsService.class);
-        RestTemplate restTemplate = mock(RestTemplate.class);
-
         ElasticSearchBase.SearchResult<StacCollectionModel> result = new ElasticSearchBase.SearchResult<>();
         result.setCollections(Collections.emptyList());
         when(mockSearch.searchCollections(anyString())).thenReturn(result);
@@ -40,10 +62,6 @@ public class WfsServerTest {
 
     @Test
     void noWfsLinks_returnsEmptyLayers() {
-        Search mockSearch = mock(Search.class);
-        DownloadableFieldsService downloadableFieldsService = mock(DownloadableFieldsService.class);
-        RestTemplate restTemplate = mock(RestTemplate.class);
-
         StacCollectionModel model = mock(StacCollectionModel.class);
         when(model.getLinks()).thenReturn(Collections.emptyList());
 
@@ -56,5 +74,31 @@ public class WfsServerTest {
 
         List<LayerInfo> layers = Collections.singletonList(LayerInfo.builder().build());
         assertEquals(Collections.emptyList(), server.filterLayersByWfsLinks("id", layers));
+    }
+    /**
+     * The function should fine one because title name matches
+     */
+    @Test
+    void primaryTitleMatch_filtersMatchingLayers() {
+        LinkModel wfsLink = LinkModel.builder()
+                .title("test_layer")
+                .aiGroup("Data Access > wfs")
+                .href("http://example.com?wfs").build();
+
+        StacCollectionModel model = StacCollectionModel.builder().links(List.of(wfsLink)).build();
+        var layers = List.of(
+                LayerInfo.builder().title("test_layer").name("").build(),
+                LayerInfo.builder().title("other").build()
+        );
+
+        ElasticSearchBase.SearchResult<StacCollectionModel> result = new ElasticSearchBase.SearchResult<>();
+        result.setCollections(List.of(model));
+        when(mockSearch.searchCollections(anyString())).thenReturn(result);
+
+        WfsServer server = new WfsServer(mockSearch, downloadableFieldsService, restTemplate, new RestTemplateUtils(restTemplate));
+
+        List<LayerInfo> info = server.filterLayersByWfsLinks("id", layers);
+        assertEquals(1, info.size(), "Layer match");
+        assertEquals(layers.get(0), info.get(0), "Layer test_layer found");
     }
 }
