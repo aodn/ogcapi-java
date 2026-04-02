e8f00af2807b5b989abd7866d6ff172589ddf9e4
Yuxuan HU <lydiahu_hn@hotmail.com>
2026-04-02 17:05:24 +1100

    add ai:role in link

--- server/src/test/java/au/org/aodn/ogcapi/server/core/mapper/StacToCollectionTest.java
+++ server/src/test/java/au/org/aodn/ogcapi/server/core/mapper/StacToCollectionTest.java
@@ -161,6 +161,13 @@ public class StacToCollectionTest {
         Assertions.assertEquals(parameterVocabs, collection.getProperties().get(CollectionProperty.parameterVocabs));
         Assertions.assertNotNull(collection.getLinks());
         Assertions.assertEquals(3, collection.getLinks().size());
+
+        ExtendedLink convertedLink1 = (ExtendedLink) collection.getLinks().stream()
+                .filter(l -> "related".equals(l.getRel()))
+                .findFirst()
+                .orElseThrow();
+
+        Assertions.assertEquals(List.of("download"), convertedLink1.getAiRole());
     }
 
     @Test
