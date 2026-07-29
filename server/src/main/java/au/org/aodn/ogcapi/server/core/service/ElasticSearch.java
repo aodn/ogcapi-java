ab4d97eb9d53ad1eb54c2411476f50d8742bc17b
Yuxuan HU <lydiahu_hn@hotmail.com>
2026-07-29 10:09:32 +1000

    add dataset_group in query clause

--- server/src/main/java/au/org/aodn/ogcapi/server/core/service/ElasticSearch.java
+++ server/src/main/java/au/org/aodn/ogcapi/server/core/service/ElasticSearch.java
@@ -311,6 +311,8 @@ public class ElasticSearch extends ElasticSearchBase implements Search {
                 should.add(CQLFields.acronym_desc.getPropertyEqualToQuery(term));
                 // credit_contains uses match query by default, exact match is not applied here
                 should.add(CQLFields.credit_contains.getPropertyEqualToQuery(term));
+                // match the acronym for AODN partner organisations
+                should.add(CQLFields.getDatasetGroupTextSearchQuery(term, isExact));
             }
         }
 
@@ -418,7 +420,7 @@ public class ElasticSearch extends ElasticSearchBase implements Search {
                     // credit_contains uses match query by default, exact match is not applied here
                     should.add(CQLFields.credit_contains.getPropertyEqualToQuery(term));
                     // match the acronym for AODN partner organisations
-                    should.add(CQLFields.dataset_group.getPropertyEqualToQuery(term));
+                    should.add(CQLFields.getDatasetGroupTextSearchQuery(term, isExact));
                 }
             }
 
